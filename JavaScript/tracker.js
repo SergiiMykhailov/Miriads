1803468726057d4e082e9200e57fb27a11b613a1
Serg Mykhailov <serg.mykhailov@gmail.com>
2024-02-12 17:53:32 +0200

    Update logging

--- JavaScript/tracker.js
+++ JavaScript/tracker.js
@@ -1,24 +1,25 @@
-(function() {
-    // Функция для отслеживания изменений в полях ввода
-    function trackInputChanges() {
-        // Получаем все элементы ввода на странице
-        var inputFields = document.querySelectorAll('input');
+function miriads_trackInputChanges() {
+    console.log('[MIRIADS] - Enumerating all input fields...');
 
-        // Для каждого поля ввода подписываемся на событие изменения
-        inputFields.forEach(function(input) {
-            input.addEventListener('input', function(event) {
-                // Получаем значение поля ввода, которое изменилось
-                var value = event.target.value;
-                // Действие при изменении значения (можно заменить на другое действие)
-                console.log('Updated input field:', value);
-            });
+    // Получаем все элементы ввода на странице
+    var inputFields = document.querySelectorAll('input');
+
+    // Для каждого поля ввода подписываемся на событие изменения
+    inputFields.forEach(function(input) {
+        input.addEventListener('input', function(event) {
+            // Получаем значение поля ввода, которое изменилось
+            var value = event.target.value;
+            // Действие при изменении значения (можно заменить на другое действие)
+            console.log('Updated input field:', value);
         });
-    }
-
-    // Вызываем функцию отслеживания изменений при загрузке страницы
-    document.addEventListener('DOMContentLoaded', function() {
-        console.log('Miriads tracking script loaded. Starting tracking...', value);
-
-        trackInputChanges();
     });
-})();
+}
+
+console.log('[MIRIADS] - Tracking script loaded. Registering for DOM loaded...');
+
+// Вызываем функцию отслеживания изменений при загрузке страницы
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('[MIRIADS] - Page DOM loaded. Starting tracking...');
+
+    miriads_trackInputChanges();
+});
