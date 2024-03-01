d6ac0513acb05f12953f8068bc3fb87b9d0bcb45
Serg Mykhailov <serg.mykhailov@gmail.com>
2024-03-01 14:33:42 +0200

    Implement Sign In with Google

--- apps/myriads/lib/ui/screens/home_screen.dart
+++ apps/myriads/lib/ui/screens/home_screen.dart
@@ -1,58 +1,100 @@
 import 'package:flutter/cupertino.dart';
+import 'package:myriads/firestore/firestore_client.dart';
 import 'package:myriads/ui/widgets/wallets_list_widget.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({
+    super.key,
+    required String userEmail
+  })
+    : _userEmail = userEmail;
+
+  // Overridden methods
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  // ignore: no_logic_in_create_state
+  State<HomeScreen> createState() => _HomeScreenState(userEmail: _userEmail);
+
+  // Internal fields
+
+  final String _userEmail;
 }
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  // Public methods and properties
+
+  _HomeScreenState({
+    required String userEmail
+  })
+    : _userEmail = userEmail {
+    _reload();
+  }
+
+  // Overridden methods
+
   @override
   Widget build(BuildContext context) {
+    Widget contentLayer = Column(
+      children: [
+        SizedBox(
+          height: 100,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Domain: ${_domain != null ? _domain! : ''}'),
+            ],
+          ),
+        ),
+        Container(height: 1, color: CupertinoColors.systemGrey5),
+        _walletsListWidget
+      ],
+    );
+
+    List<Widget> layers = [contentLayer];
+    if (_isLoading) {
+      layers.add(
+        Container(
+          color: CupertinoColors.white,
+          child: const CupertinoActivityIndicator(),
+        )
+      );
+    }
+    else if (_domain == null) {
+      layers.add(
+        Container(
+          color: CupertinoColors.white,
+          child: Center(child: Text('No domain found for user with email: $_userEmail')),
+        )
+      );
+    }
+
     return Container(
       color: CupertinoColors.white,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(child: Container()),
-                SizedBox(
-                  width: 300,
-                  child: CupertinoTextField(
-                    placeholder: 'Specify your domain',
-                    textAlign: TextAlign.center,
-                    onChanged: (String updatedValue) {
-                      _domain = updatedValue;
-                    },
-                  ),
-                ),
-                const SizedBox(width: 10),
-                CupertinoButton(
-                  child: const Text('Apply'),
-                  onPressed: () {
-                    _walletsListWidget.reload(_domain);
-                  }
-                ),
-                Expanded(child: Container()),
-              ],
-            ),
-          ),
-          Container(height: 1, color: CupertinoColors.systemGrey5),
-          _walletsListWidget
-        ],
-      ),
+      child: Stack(children: layers),
     );
+  }
+
+  // Internal methods
+
+  void _reload() async {
+    final userInfo = await FirestoreClient.loadUserInfo(_userEmail);
+
+    setState(() {
+      _isLoading = false;
+      _domain = userInfo?.domain;
+
+      if (_domain != null) {
+        _walletsListWidget.reload(_domain!);
+      }
+    });
   }
 
   // Internal fields
 
   final _walletsListWidget = WalletsListWidget();
-  String _domain = '';
+  final String _userEmail;
+  bool _isLoading = true;
+  String? _domain;
 }
