59398e0919be4a01824fe752cd3c2d2070a1af82
Serg Mykhailov <serg.mykhailov@gmail.com>
2024-04-12 16:15:20 +0200

    Implement caching wallets transactions

--- cloud/firestore_functions/functions/main.py
+++ cloud/firestore_functions/functions/main.py
@@ -5,15 +5,14 @@ from firebase_admin import initialize_app, firestore
 import google.cloud.firestore
 import time
 
+MORALIS_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjI2OWIwMzI1LWViMjctNDA5Ni1iY2VjLTYxMGQ4YmJmYjRiYyIsIm9yZ0lkIjoiMzgyNDQyIiwidXNlcklkIjoiMzkyOTY0IiwidHlwZUlkIjoiMDU5MWQ5NzItZGU3OC00ZjNjLTllMTYtNzU4OGQ1NTJjMjg2IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MTAyNDkzNjcsImV4cCI6NDg2NjAwOTM2N30.u0OgFrKWer9nuFq2cimZZfv4CNgeqeYlZxCEpWDHrSM"
+
 
 initialize_app()
 
 
 @https_fn.on_request()
 def record_entry(request: https_fn.Request) -> https_fn.Response:
-    """Take the text parameter passed to this HTTP endpoint and insert it into
-    a new document in the messages collection."""
-    # Grab the text parameter.
     user_id = request.args.get("user_id")
     wallet_id = request.args.get("wallet_id")
     domain = request.args.get("domain")
@@ -33,6 +32,11 @@ def record_entry(request: https_fn.Request) -> https_fn.Response:
         .collection("visitors") \
         .document(user_id) \
         .set({"updated_at": current_timestamp})
+
+    firestore_client \
+        .collection("domains") \
+        .document(domain) \
+        .set({"updated_at": current_timestamp}, merge=True)
 
     session_data = {"wallet_id": wallet_id}
     utm_source = request.args.get("utm_source")
@@ -58,3 +62,110 @@ def record_entry(request: https_fn.Request) -> https_fn.Response:
 
     # Send back a message that we've successfully written the message
     return https_fn.Response("Record added.")
+
+
+
+@https_fn.on_request()
+def update_wallets(request: https_fn.Request) -> https_fn.Response:
+    firestore_client: google.cloud.firestore.Client = firestore.client()
+    current_timestamp = time.time()
+
+    response = ""
+
+    try:
+        domains = firestore_client.collection("domains").stream()
+
+        wallets = set()
+
+        for domain in domains:
+            response += "\n--Processing domain: " + domain.id
+
+            visitors = firestore_client \
+                .collection("domains") \
+                .document(domain.id) \
+                .collection("visitors") \
+                .stream()
+
+            for visitor in visitors:
+                response += "\n----Processing visitor: " + visitor.id
+
+                sessions = firestore_client \
+                    .collection("domains") \
+                    .document(domain.id) \
+                    .collection("visitors") \
+                    .document(visitor.id) \
+                    .collection("sessions") \
+                    .stream()
+
+                for session in sessions:
+                    session_data = session.to_dict()
+                    response += "\n------Processing session: " + session.id + " session data: " + str(session_data)
+
+                    wallet_id = session_data["wallet_id"]
+                    response += "\n------Processing session: " + session.id + " wallet_id: " + wallet_id
+
+                    wallets.add(wallet_id)
+
+        response += "\nAll wallets: " + str(wallets)
+
+        try:
+            import moralis
+        except ImportError:
+            response += "\nInstalling Moralis SDK..."
+            try:
+                import pip
+                pip.main(['install', 'moralis'])
+                response += "\nMoralis SDK installation succeeded"
+            except Exception as e:
+                response += "\nMoralis SDK installation failed"
+
+        from moralis import evm_api
+
+        for wallet in wallets:
+            cursor = None
+
+            while True:
+                params = {
+                    "chain": "eth",
+                    "order": "DESC",
+                    "address": wallet
+                }
+
+                if cursor is not None and cursor.strip():
+                    params["cursor"] = cursor
+
+                wallet_history_response = evm_api.wallets.get_wallet_history(
+                    api_key=MORALIS_API_KEY,
+                    params=params,
+                )
+
+                cursor = wallet_history_response["cursor"]
+
+                response += f"\n\n\nTransactions fetch result for wallet [{wallet}]: \n" + str(wallet_history_response)
+                response += f"\nCursor: [{cursor}]:"
+
+                transactions_data = wallet_history_response["result"]
+                for transaction_entry in transactions_data:
+                    transaction_hash = transaction_entry["hash"]
+
+                    firestore_client \
+                        .collection("wallets") \
+                        .document(wallet) \
+                        .collection("transactions") \
+                        .document(transaction_hash) \
+                        .set(transaction_entry)
+
+                if cursor is None or not cursor.strip():
+                    firestore_client \
+                        .collection("wallets") \
+                        .document(wallet) \
+                        .set({"updated_at": current_timestamp}, merge=True)
+                    break
+
+        response += "\n\n\nWallets updated"
+    except Exception as e:
+        response += "\n" + str(e)
+
+    response = "<pre>" + response + "</pre>"
+
+    return https_fn.Response(response)
