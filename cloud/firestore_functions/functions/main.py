05b5a3edc8da9cdae71ef4b1850bf559d94a83ee
Serg Mykhailov <serg.mykhailov@gmail.com>
2024-04-18 15:33:21 +0200

    Split updating wallet balance and wallet transactions into 2 endpoints

--- cloud/firestore_functions/functions/main.py
+++ cloud/firestore_functions/functions/main.py
@@ -26,18 +26,18 @@ def record_entry(request: https_fn.Request) -> https_fn.Response:
 
     firestore_client: google.cloud.firestore.Client = firestore.client()
 
-    current_timestamp = time.time()
+    start_timestamp = time.time()
     firestore_client \
         .collection("domains") \
         .document(domain) \
         .collection("visitors") \
         .document(user_id) \
-        .set({"updated_at": current_timestamp})
+        .set({"updated_at": start_timestamp})
 
     firestore_client \
         .collection("domains") \
         .document(domain) \
-        .set({"updated_at": current_timestamp}, merge=True)
+        .set({"updated_at": start_timestamp}, merge=True)
 
     session_data = {"wallet_id": wallet_id}
     utm_source = request.args.get("utm_source")
@@ -66,63 +66,74 @@ def record_entry(request: https_fn.Request) -> https_fn.Response:
 
 
 
-@https_fn.on_request()
-def update_wallets(request: https_fn.Request) -> https_fn.Response:
+def load_all_wallets_from_visitors_sessions() -> set[str]:
     firestore_client: google.cloud.firestore.Client = firestore.client()
-    current_timestamp = time.time()
 
-    response = ""
+    domains = firestore_client.collection("domains").stream()
 
-    try:
-        domains = firestore_client.collection("domains").stream()
+    wallets = set()
 
-        wallets = set()
+    for domain in domains:
+        visitors = firestore_client \
+            .collection("domains") \
+            .document(domain.id) \
+            .collection("visitors") \
+            .stream()
 
-        for domain in domains:
-            response += "\n--Processing domain: " + domain.id
-
-            visitors = firestore_client \
+        for visitor in visitors:
+            sessions = firestore_client \
                 .collection("domains") \
                 .document(domain.id) \
                 .collection("visitors") \
+                .document(visitor.id) \
+                .collection("sessions") \
                 .stream()
 
-            for visitor in visitors:
-                response += "\n----Processing visitor: " + visitor.id
+            for session in sessions:
+                session_data = session.to_dict()
+                wallet_id = session_data["wallet_id"]
 
-                sessions = firestore_client \
-                    .collection("domains") \
-                    .document(domain.id) \
-                    .collection("visitors") \
-                    .document(visitor.id) \
-                    .collection("sessions") \
-                    .stream()
+                wallets.add(wallet_id)
 
-                for session in sessions:
-                    session_data = session.to_dict()
-                    response += "\n------Processing session: " + session.id + " session data: " + str(session_data)
+    return wallets
 
-                    wallet_id = session_data["wallet_id"]
-                    response += "\n------Processing session: " + session.id + " wallet_id: " + wallet_id
 
-                    wallets.add(wallet_id)
 
-        response += "\nAll wallets: " + str(wallets)
+def install_moralis_if_needed() -> str:
+    response = ""
 
+    try:
+        import moralis
+    except ImportError:
+        response += "\nInstalling Moralis SDK..."
         try:
-            import moralis
-        except ImportError:
-            response += "\nInstalling Moralis SDK..."
-            try:
-                import pip
-                pip.main(['install', 'moralis'])
-                response += "\nMoralis SDK installation succeeded"
-            except Exception as e:
-                response += "\nMoralis SDK installation failed"
+            import pip
+            pip.main(['install', 'moralis'])
+            response += "\nMoralis SDK installation succeeded"
+        except Exception as e:
+            response += "\nMoralis SDK installation failed"
 
+    return response
+
+
+
+@https_fn.on_request()
+def update_wallets_transactions(request: https_fn.Request) -> https_fn.Response:
+    response = ""
+
+    try:
+        firestore_client: google.cloud.firestore.Client = firestore.client()
+        start_timestamp = time.time()
+
+        wallets = load_all_wallets_from_visitors_sessions()
+        response += "\nAll wallets: " + str(wallets) + "\n"
+
+        response += install_moralis_if_needed()
         from moralis import evm_api
 
         for wallet in wallets:
+            response += f"\n\n\n>>> BEGIN processing wallet [{wallet}]: \n"
+
             cursor = None
 
             while True:
@@ -132,17 +143,21 @@ def update_wallets(request: https_fn.Request) -> https_fn.Response:
                     "address": wallet
                 }
 
+                response += f">>>>>> Handling cursor: [{cursor}]: \n"
                 if cursor is not None and cursor.strip():
+                    response += f">>>>>>>>> Cursor is set \n"
                     params["cursor"] = cursor
+                response += f"<<<<<< Handling cursor: [{cursor}]: \n"
 
+                response += f">>>>>> Fetching transactions history... \n"
                 wallet_history_response = evm_api.wallets.get_wallet_history(
                     api_key=MORALIS_API_KEY,
                     params=params,
                 )
-
-                cursor = wallet_history_response["cursor"]
-
+                response += f"<<<<<< Fetching transactions history... \n"
                 response += f"\n\n\nTransactions fetch result for wallet [{wallet}]: \n" + str(wallet_history_response)
+
+                cursor = wallet_history_response.get("cursor")
                 response += f"\nCursor: [{cursor}]:"
 
                 transactions_data = wallet_history_response["result"]
@@ -160,8 +175,37 @@ def update_wallets(request: https_fn.Request) -> https_fn.Response:
                     firestore_client \
                         .collection("wallets") \
                         .document(wallet) \
-                        .set({"updated_at": current_timestamp}, merge=True)
+                        .set({"transactions_updated_at": start_timestamp}, merge=True)
                     break
+
+            response += f"\n\n\n<<< END processing wallet [{wallet}]: \n"
+
+        response += "\n\n\nWallets updated"
+    except Exception as e:
+        response += "\nException occurred: " + str(e)
+
+    response = "<pre>\n\n" + response + "\n\n</pre>\n"
+
+    return https_fn.Response(response)
+
+
+
+@https_fn.on_request()
+def update_wallets_balance(request: https_fn.Request) -> https_fn.Response:
+    response = ""
+
+    try:
+        firestore_client: google.cloud.firestore.Client = firestore.client()
+        start_timestamp = time.time()
+
+        wallets = load_all_wallets_from_visitors_sessions()
+        response += "\nAll wallets: " + str(wallets) + "\n"
+
+        response += install_moralis_if_needed()
+        from moralis import evm_api
+
+        for wallet in wallets:
+            response += f"\n\n\n>>> BEGIN processing wallet [{wallet}]: \n"
 
             params = {
                 "exclude_spam": True,
@@ -178,15 +222,21 @@ def update_wallets(request: https_fn.Request) -> https_fn.Response:
                 .collection("wallets") \
                 .document(wallet) \
                 .collection("balance_history") \
-                .document(str(current_timestamp)) \
+                .document(str(start_timestamp)) \
                 .set(wallet_net_worth_in_usd_response, merge=True)
 
+            firestore_client \
+                .collection("wallets") \
+                .document(wallet) \
+                .set({"balance_updated_at": start_timestamp}, merge=True)
+
             response += f"\n\n\nNet-worth fetch result for wallet [{wallet}]: \n" + str(wallet_net_worth_in_usd_response)
+            response += f"\n\n\n<<< END processing wallet [{wallet}]: \n"
 
         response += "\n\n\nWallets updated"
     except Exception as e:
-        response += "\n" + str(e)
+        response += "\nException occurred: " + str(e)
 
-    response = "<pre>" + response + "</pre>"
+    response = "<pre>\n\n" + response + "\n\n</pre>\n"
 
     return https_fn.Response(response)
