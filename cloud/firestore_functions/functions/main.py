5edf1b1a0c860029d53834872ea961714a421f9b
Serg Mykhailov <serg.mykhailov@gmail.com>
2024-04-21 15:55:07 +0300

    Implement wallets data caching

--- cloud/firestore_functions/functions/main.py
+++ cloud/firestore_functions/functions/main.py
@@ -7,6 +7,7 @@ import time
 
 
 MORALIS_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjI2OWIwMzI1LWViMjctNDA5Ni1iY2VjLTYxMGQ4YmJmYjRiYyIsIm9yZ0lkIjoiMzgyNDQyIiwidXNlcklkIjoiMzkyOTY0IiwidHlwZUlkIjoiMDU5MWQ5NzItZGU3OC00ZjNjLTllMTYtNzU4OGQ1NTJjMjg2IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MTAyNDkzNjcsImV4cCI6NDg2NjAwOTM2N30.u0OgFrKWer9nuFq2cimZZfv4CNgeqeYlZxCEpWDHrSM"
+MAX_REQUESTS_PER_BATCH = 50
 
 
 initialize_app()
@@ -152,60 +153,67 @@ def install_moralis_if_needed() -> str:
 def update_transactions_of_wallets(wallets) -> str:
     response = ""
 
-    firestore_client: google.cloud.firestore.Client = firestore.client()
+    try:
+        firestore_client: google.cloud.firestore.Client = firestore.client()
 
-    response += install_moralis_if_needed()
-    from moralis import evm_api
+        response += install_moralis_if_needed()
+        from moralis import evm_api
 
-    for wallet in wallets:
-        response += f"\n\n\n>>> BEGIN processing wallet [{wallet}]: \n"
+        requests_count = 0
 
-        cursor = None
+        for wallet in wallets:
+            response += f"\n\n\n>>> BEGIN processing wallet [{wallet}]: \n"
 
-        while True:
-            params = {
-                "chain": "eth",
-                "order": "DESC",
-                "address": wallet
-            }
+            cursor = None
 
-            response += f">>>>>> Handling cursor: [{cursor}]: \n"
-            if cursor is not None and cursor.strip():
-                response += f">>>>>>>>> Cursor is set \n"
-                params["cursor"] = cursor
-            response += f"<<<<<< Handling cursor: [{cursor}]: \n"
+            while True:
+                params = {
+                    "chain": "eth",
+                    "order": "DESC",
+                    "address": wallet
+                }
 
-            response += f">>>>>> Fetching transactions history... \n"
-            wallet_history_response = evm_api.wallets.get_wallet_history(
-                api_key=MORALIS_API_KEY,
-                params=params,
-            )
-            response += f"<<<<<< Fetching transactions history... \n"
-            response += f"\n\n\nTransactions fetch result for wallet [{wallet}]: \n" + str(wallet_history_response)
+                response += f">>>>>> Handling cursor: [{cursor}]: \n"
+                if cursor is not None and cursor.strip():
+                    response += f">>>>>>>>> Cursor is set \n"
+                    params["cursor"] = cursor
+                response += f"<<<<<< Handling cursor: [{cursor}]: \n"
 
-            cursor = wallet_history_response.get("cursor")
-            response += f"\nCursor: [{cursor}]:"
+                response += f">>>>>> Fetching transactions history... \n"
+                wallet_history_response = evm_api.wallets.get_wallet_history(
+                    api_key=MORALIS_API_KEY,
+                    params=params,
+                )
+                response += f"<<<<<< Fetching transactions history... \n"
+                response += f"\n\n\nTransactions fetch result for wallet [{wallet}]: \n" + str(wallet_history_response)
 
-            transactions_data = wallet_history_response["result"]
-            for transaction_entry in transactions_data:
-                transaction_hash = transaction_entry["hash"]
+                requests_count += 1
 
-                firestore_client \
-                    .collection("wallets") \
-                    .document(wallet) \
-                    .collection("transactions") \
-                    .document(transaction_hash) \
-                    .set(transaction_entry)
+                cursor = wallet_history_response.get("cursor")
+                response += f"\nCursor: [{cursor}]:"
 
-            if cursor is None or not cursor.strip():
-                current_timestamp = time.time()
-                firestore_client \
-                    .collection("wallets") \
-                    .document(wallet) \
-                    .set({"transactions_updated_at": current_timestamp}, merge=True)
-                break
+                transactions_data = wallet_history_response["result"]
+                for transaction_entry in transactions_data:
+                    transaction_hash = transaction_entry["hash"]
 
-        response += f"\n\n\n<<< END processing wallet [{wallet}]: \n"
+                    firestore_client \
+                        .collection("wallets") \
+                        .document(wallet) \
+                        .collection("transactions") \
+                        .document(transaction_hash) \
+                        .set(transaction_entry)
+
+                if cursor is None or not cursor.strip() or requests_count >= MAX_REQUESTS_PER_BATCH:
+                    current_timestamp = time.time()
+                    firestore_client \
+                        .collection("wallets") \
+                        .document(wallet) \
+                        .set({"transactions_updated_at": current_timestamp}, merge=True)
+                    break
+
+            response += f"\n\n\n<<< END processing wallet [{wallet}]: \n"
+    except Exception as e:
+        response += "\nException occurred: " + str(e)
 
     return response
     
@@ -267,40 +275,50 @@ def update_wallets_transactions(request: https_fn.Request) -> https_fn.Response:
 def update_balance_of_wallets(wallets) -> str:
     response = ""
 
-    firestore_client: google.cloud.firestore.Client = firestore.client()
+    try:
+        firestore_client: google.cloud.firestore.Client = firestore.client()
 
-    response += install_moralis_if_needed()
-    from moralis import evm_api
+        requests_count = 0
 
-    for wallet in wallets:
-        response += f"\n\n\n>>> BEGIN processing wallet [{wallet}]: \n"
+        response += install_moralis_if_needed()
+        from moralis import evm_api
 
-        params = {
-            "exclude_spam": True,
-            "exclude_unverified_contracts": True,
-            "address": wallet
-        }
+        for wallet in wallets:
+            response += f"\n\n\n>>> BEGIN processing wallet [{wallet}]: \n"
 
-        wallet_net_worth_in_usd_response = evm_api.wallets.get_wallet_net_worth(
-            api_key=MORALIS_API_KEY,
-            params=params,
-        )
+            params = {
+                "exclude_spam": True,
+                "exclude_unverified_contracts": True,
+                "address": wallet
+            }
 
-        current_timestamp = time.time()
-        firestore_client \
-            .collection("wallets") \
-            .document(wallet) \
-            .collection("balance_history") \
-            .document(str(current_timestamp)) \
-            .set(wallet_net_worth_in_usd_response, merge=True)
+            wallet_net_worth_in_usd_response = evm_api.wallets.get_wallet_net_worth(
+                api_key=MORALIS_API_KEY,
+                params=params,
+            )
 
-        firestore_client \
-            .collection("wallets") \
-            .document(wallet) \
-            .set({"balance_updated_at": current_timestamp}, merge=True)
+            current_timestamp = time.time()
+            firestore_client \
+                .collection("wallets") \
+                .document(wallet) \
+                .collection("balance_history") \
+                .document(str(current_timestamp)) \
+                .set(wallet_net_worth_in_usd_response, merge=True)
 
-        response += f"\n\n\nNet-worth fetch result for wallet [{wallet}]: \n" + str(wallet_net_worth_in_usd_response)
-        response += f"\n\n\n<<< END processing wallet [{wallet}]: \n"
+            firestore_client \
+                .collection("wallets") \
+                .document(wallet) \
+                .set({"balance_updated_at": current_timestamp}, merge=True)
+
+            response += f"\n\n\nNet-worth fetch result for wallet [{wallet}]: \n" + str(wallet_net_worth_in_usd_response)
+            response += f"\n\n\n<<< END processing wallet [{wallet}]: \n"
+
+            requests_count += 1
+            if requests_count >= MAX_REQUESTS_PER_BATCH:
+                response += f"\nExceeded max requests count per batch ({MAX_REQUESTS_PER_BATCH}). Terminating...\n"
+                return response
+    except Exception as e:
+        response += "\nException occurred: " + str(e)
 
     return response
 
@@ -330,6 +348,93 @@ def update_wallets_balance(request: https_fn.Request) -> https_fn.Response:
         response += "\nWallets addresses sorted by balance update time: " + str(existing_wallets_addresses_sorted) + "\n"
 
         response += update_balance_of_wallets(existing_wallets_addresses_sorted)
+
+        response += "\n\n\nWallets updated"
+    except Exception as e:
+        response += "\nException occurred: " + str(e)
+
+    response = "<pre>\n\n" + response + "\n\n</pre>\n"
+
+    return https_fn.Response(response)
+
+
+
+def update_statistics_of_wallets(wallets) -> str:
+    response = ""
+
+    try:
+        firestore_client: google.cloud.firestore.Client = firestore.client()
+
+        requests_count = 0
+
+        response += install_moralis_if_needed()
+        from moralis import evm_api
+
+        for wallet in wallets:
+            response += f"\n\n\n>>> BEGIN processing wallet [{wallet}]: \n"
+
+            params = {
+              "chain": "eth",
+              "address": wallet
+            }
+
+            wallet_statistics_response = evm_api.wallets.get_wallet_stats(
+              api_key=MORALIS_API_KEY,
+              params=params,
+            )
+
+            current_timestamp = time.time()
+            firestore_client \
+                .collection("wallets") \
+                .document(wallet) \
+                .collection("statistics_history") \
+                .document(str(current_timestamp)) \
+                .set(wallet_statistics_response, merge=True)
+
+            firestore_client \
+                .collection("wallets") \
+                .document(wallet) \
+                .set({"statistics_updated_at": current_timestamp}, merge=True)
+
+            response += f"\n\n\nStatistics fetch result for wallet [{wallet}]: \n" + str(wallet_statistics_response)
+            response += f"\n\n\n<<< END processing wallet [{wallet}]: \n"
+
+            requests_count += 1
+            if requests_count >= MAX_REQUESTS_PER_BATCH:
+                response += f"\nExceeded max requests count per batch ({MAX_REQUESTS_PER_BATCH}). Terminating...\n"
+                return response
+
+    except Exception as e:
+            response += "\nException occurred: " + str(e)
+
+    return response
+
+
+
+@https_fn.on_request()
+def update_wallets_statistics(request: https_fn.Request) -> https_fn.Response:
+    response = ""
+
+    try:
+        wallets = load_all_wallets_from_visitors_sessions()
+        response += "\nAll wallets: " + str(wallets) + "\n"
+
+        existing_wallets_updates = load_all_existing_wallets_updates()
+        fresh_wallets = find_fresh_wallets(wallets, existing_wallets_updates)
+
+        response += "\nFresh wallets: " + str(fresh_wallets) + "\n"
+        response += update_statistics_of_wallets(fresh_wallets)
+
+        existing_wallets_sorted = sorted(
+            existing_wallets_updates,
+            key = lambda element: element.balance_update_timestamp
+        )
+        response += "\nWallets sorted by statistics update time: " + str(existing_wallets_sorted) + "\n"
+
+        existing_wallets_addresses_sorted = [element.wallet_address for element in existing_wallets_sorted]
+        response += "\nWallets addresses sorted by statistics update time: " + str(existing_wallets_addresses_sorted) + "\n"
+
+        response += update_statistics_of_wallets(existing_wallets_addresses_sorted)
 
         response += "\n\n\nWallets updated"
     except Exception as e:
